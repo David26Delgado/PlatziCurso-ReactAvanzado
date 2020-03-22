921fb40d52ab45d1ddbfd67724fa606401e83a5b
David Delgado <dadch13.14@gmail.com>
2020-03-22 18:29:36 -0400

    ¿Qué son los Hooks?

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // Components
 import { Category } from '../Category'
@@ -6,10 +6,17 @@ import { Category } from '../Category'
 // Styled Components
 import { List, Item } from './styles'
 
-// Api
-import { categories } from './../../../api/db'
-
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    window.fetch('https://petgram-server-ddelgado-5xwsbs4j9.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {categories.map((category) => <Item key={category.id}><Category {...category} /></Item>)}
