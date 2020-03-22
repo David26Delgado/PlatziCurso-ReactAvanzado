c3a55f24ac9d27839663b683f1820dcbf6fac7f5
David Delgado <dadch13.14@gmail.com>
2020-03-22 18:48:37 -0400

    useEffect: limpiando eventos

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -4,6 +4,20 @@ export const List = styled.ul`
   display:flex;
   overflow: scroll;
   width: 100%;
+  &.fixed {
+    background-color: #fff;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    position:fixed;
+    top: -20px;
+    right: 0;
+    left: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    transform: scale(.5);
+    z-index: 1;
+  }
 `
 
 export const Item = styled.li`
