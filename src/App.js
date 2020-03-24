2107bb9aaca1623c7cf4ec868be7ed60dc3848e8
David Delgado <dadch13.14@gmail.com>
2020-03-24 11:09:11 -0400

    Rutas protegidas.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,17 @@ import { NavBar } from './components/NavBar'
 // Pages
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 // Styled Components
 import { GlobalStyles } from './styles/GlobalStyles'
+
+// Constants
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -24,6 +32,24 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? (
+                <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+              )
+              : (
+                <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+              )
+        }
+      </UserLogged>
       <NavBar />
     </>
   )
