8632e79af241540a2ec8c013b95f90de7c3ea528
David Delgado <dadch13.14@gmail.com>
2020-03-24 10:22:07 -0400

    Usando Link para evitar recargar la página.

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 // Styled Components
-import { Anchor, Image } from './styles'
+import { Link, Image } from './styles'
 
 // Constants
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
 
-export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
-  <Anchor
-    href={path}
+export const Category = ({ cover = DEFAULT_IMAGE, path = '#', emoji = '?' }) => (
+  <Link
+    to={path}
   >
     <Image src={cover} />
     {emoji}
-  </Anchor>
+  </Link>
 )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../containers/ListOfPhotoCards'
 
-export const Home = ({id}) => {
+export const Home = ({ id }) => {
   return (
     <>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
-      </>
+    </>
   )
 }
